0d1f79f3b7ce672346c25980a53e25c44cacd151
kayot4 <egor.poliakin.4@gmail.com>
2025-09-06 06:04:01 +1100

    Add files via upload

---
+++ quick_sort.py
@@ -0,0 +1,9 @@
+def quick_sort(arr):
+    if len(arr) < 2:
+        return arr
+    pivot = arr[0]
+    less = [i for i in arr[1:] if i < pivot]
+    greater = [i for i in arr[1:] if i > pivot]
+    return quick_sort(less) + [pivot] + quick_sort(greater)
+
+print(quick_sort([10,2,7,4,5]))
